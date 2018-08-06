88e6e6fb4b56dfa848b44e232750d909548807e7
Corey Matzat <corey@thematzats.com>
2018-08-06 17:14:21 -0500

    Start basic website development
    Added sample HTML to view an example few rounds and started on the CSS for proper formatting.

--- schedule.php
+++ schedule.php
@@ -5,6 +5,42 @@
     <link rel="stylesheet" type="text/css" href="style/schedule.css"/> 
   </head>
   <body>
-    
+    <div class="schedule-container">
+      <div class="round">
+        <div class="round-header">ROUND 1</div>
+        <div class="match">SKC<br>v<br>HOU</div>
+        <div class="match">ATL<br>v<br>TOR</div>
+        <div class="match">SEA<br>v<br>MIN</div>
+        <div class="match">DAL<br>v<br>SJ</div>
+        <div class="match">LA FC<br>v<br>NYC</div>
+        <div class="match">LAG<br>v<br>ORL</div>
+        <div class="match">COL<br>v<br>RSL</div>
+        <div class="match">VAN<br>v<br>MTL</div>
+      </div>
+      <div class="round">
+        <div class="round-header">ROUND 2</div>
+        <div class="match">TOR<br>v<br>SEA</div>
+        <div class="match">ATL<br>v<br>CLB</div>
+        <div class="match">NYC<br>v<br>NY</div>
+        <div class="match">ORL<br>v<br>DAL</div>
+        <div class="match">MIN<br>v<br>CHI</div>
+        <div class="match">NE<br>v<br>SKC</div>
+        <div class="match">POR<br>v<br>RSL</div>
+        <div class="match">MTL<br>v<br>COL</div>
+        <div class="match">LAG<br>v<br>SJ</div>
+        <div class="match">HOU<br>v<br>DC</div>
+        <div class="match">LA FC<br>v<br>VAN</div>
+      </div>
+      <div class="round">
+        <div class="round-header">ROUND 3</div>
+        <div class="match">NE<br>v<br>LAG</div>
+        <div class="match">CHI<br>v<br>COL</div>
+        <div class="match">NY<br>v<br>DC</div>
+        <div class="match">HOU<br>v<br>DAL</div>
+        <div class="match">ORL<br>v<br>MTL</div>
+        <div class="match">SKC<br>v<br>MIN</div>
+        <div class="match">SJ<br>v<br>POR</div>
+      </div>
+    </div>
   </body>
 </html>
