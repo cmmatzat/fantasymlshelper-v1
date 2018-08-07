e6697672e7174da3777558ed78b3212376bed7dd
Corey Matzat <corey@thematzats.com>
2018-08-07 11:47:45 -0500

    Change index to php and add nav
    Make the homepage a php file for dynamic content.
    
    Add a navbar and respective css.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		  <title>FMH - Schedule</title>
+      <link rel="stylesheet" type="text/css" href="style/home.css"/>
+      <link rel="stylesheet" type="text/css" href="style/nav.css"/>
+	</head>
+	<body>
+    <nav>
+      <div><a href="index.php">HOME</a></div>
+      <div><a href="schedule.php">SCHEDULE</a></div>
+    </nav>
+	</body>
+</html>
