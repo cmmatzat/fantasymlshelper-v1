3a3bbf7540adf99c6358d64abb144a94f2f8d106
Corey Matzat <corey@thematzats.com>
2018-08-06 22:01:36 -0500

    Add php script to dynamically add schedule
    Adds a simple php script to add in the full schedule to the
    page with round divisions. Currently, only the two teams who
    are playing are listed with no other information.

--- schedule.php
+++ schedule.php
@@ -6,41 +6,32 @@
   </head>
   <body>
     <div class="schedule-container">
+    <?php
+      $url='https://data.fantasymlshelper.com/data/schedule/schedule.json';
+      $response = get_json_data($url);
+      $schedule = json_decode($response, true);
+
+      function get_json_data($url)
+      {
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+      }
+    ?>
+
+    <?php foreach ($schedule as $round) : ?>
       <div class="round">
-        <div class="round-header">ROUND 1</div>
-        <div class="match">SKC<br>v<br>HOU</div>
-        <div class="match">ATL<br>v<br>TOR</div>
-        <div class="match">SEA<br>v<br>MIN</div>
-        <div class="match">DAL<br>v<br>SJ</div>
-        <div class="match">LA FC<br>v<br>NYC</div>
-        <div class="match">LAG<br>v<br>ORL</div>
-        <div class="match">COL<br>v<br>RSL</div>
-        <div class="match">VAN<br>v<br>MTL</div>
+        <div class="round-header">ROUND <?php echo $round['round']; ?> </div>
+      <?php foreach ($round['matches'] as $match) : ?>
+        <div class="match"><?php echo $match['home_squad']['short_name'] ?><br>v<br><?php echo $match['away_squad']['short_name'] ?></div>
+      <?php endforeach; ?>
       </div>
-      <div class="round">
-        <div class="round-header">ROUND 2</div>
-        <div class="match">TOR<br>v<br>SEA</div>
-        <div class="match">ATL<br>v<br>CLB</div>
-        <div class="match">NYC<br>v<br>NY</div>
-        <div class="match">ORL<br>v<br>DAL</div>
-        <div class="match">MIN<br>v<br>CHI</div>
-        <div class="match">NE<br>v<br>SKC</div>
-        <div class="match">POR<br>v<br>RSL</div>
-        <div class="match">MTL<br>v<br>COL</div>
-        <div class="match">LAG<br>v<br>SJ</div>
-        <div class="match">HOU<br>v<br>DC</div>
-        <div class="match">LA FC<br>v<br>VAN</div>
-      </div>
-      <div class="round">
-        <div class="round-header">ROUND 3</div>
-        <div class="match">NE<br>v<br>LAG</div>
-        <div class="match">CHI<br>v<br>COL</div>
-        <div class="match">NY<br>v<br>DC</div>
-        <div class="match">HOU<br>v<br>DAL</div>
-        <div class="match">ORL<br>v<br>MTL</div>
-        <div class="match">SKC<br>v<br>MIN</div>
-        <div class="match">SJ<br>v<br>POR</div>
-      </div>
+    <?php endforeach; ?>
     </div>
   </body>
 </html>
